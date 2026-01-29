bb187865041a3fca4093b814f0beb51cb85884bd
Your Name <you@example.com>
2026-01-29 22:51:15 +0330

    add serves and template in project

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,14 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/test',
-      name: 'test',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('/src/components/test.vue'),
-    },
   ],
 })
 
